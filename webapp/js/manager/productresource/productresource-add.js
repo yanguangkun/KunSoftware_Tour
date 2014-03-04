45899a6338e77622feb42d550e46b70aaad42e22
yanguangkun <yanguangkun@126.com>
2014-03-04 17:55:49 +0800

    新增模块

---
+++ webapp/js/manager/productresource/productresource-add.js
@@ -0,0 +1,46 @@
+define(function(require, exports, module) {
+	 
+	var $ = require('jquery'); 
+	require('bootstrap')($); 
+	require('jquery-form')($); 
+	require('jquery-validate')($);
+	require('jquery-validate-messages')($); 
+	require('cascade');
+	var lockscreen = require('lockscreen');
+	var modaldialog = require('modaldialog'); 
+	
+	$(document).ready(function(){ 
+		
+		var validate =  $("#saveFrm").validate({ 
+			submitHandler: function(form) { 
+				$(form).ajaxSubmit({
+					dataType:'json', 
+					success:function(data) {
+						alert(data.message);
+						if(data.success == '1') {
+							$("#id").val(data.id);
+						}
+						lockscreen.unLock();
+					}
+				});
+			}	
+		}); 
+		
+		$(".saveBtn").click(function(e) {  
+			var check = validate.form();  
+			if(check) {
+				lockscreen.lock();
+			} 
+			$("#saveFrm").submit();
+        });
+		
+		$("#destinationImageFile").change(function(e) { 
+            $("#imagePathShow").attr("src",$(this).val());
+        });
+		
+		$(".dialogBtn").click(function(e) { 
+			modaldialog({code:'gallery',obj:this});
+		}); 
+	}); 
+	 
+});
