a3e3cd3f76f1dce9893ac75a6d4d4a28d7d6f683
yanguangkun <yanguangkun@126.com>
2014-05-08 16:10:47 +0800

    新增模块

--- webapp/js/front/photoalbum-list.js
+++ webapp/js/front/photoalbum-list.js
@@ -10,34 +10,6 @@ define(function(require, exports, module) {
 	$(document).ready(function(){  
 		$(".destination").click(function(e) {
             location.href = "list?destination=" + $(this).attr("value");
-        }); 
-		
-		$("#phoneModel").click(function(e) {
-            $(this).hide();
-			$(".photoalbumContent").hide();
-        });
-		
-		$(document).on("click",".photoalbumTarget .photoalbumTimeline .s5",function() {
-			$(".photoalbumBody .timeline-content-item").hide();
-			var target = $(this).attr("value");
-			$(".photoalbumTarget .photoalbumTimeline .s5").removeClass("cur");
-			$(this).addClass("cur");
-			
-			$(".photoalbumBody").find(target).show();
-		});
-		 
-		$(".photo_item").click(function(e) {
-            
-			$("#phoneModel").addClass("in").show();
-			var obj = $(".photoalbumContent");//获得相应的Div对象  
-			var x = ($(window).width()- 950)/2;//使用$(window).width()获得显示器的宽，并算出对应的Div离左边的距离  
-			var y = ($(window).height()- 570)/2;//使用$(window).height()获得显示器的高，并算出相应的Div离上边的距离  
-			obj.css("top",y).css("left",x);
-			
-		  	//alert($(this).find(".timeline").html());
-			$(".photoalbumBody").html($(this).find(".timeline-content").html());
-			$(".photoalbumTimeline").html($(this).find(".timeline").html());
-			obj.show();
         });
 	}); 
 	
