874fadb5dd8a32bcb63eddfcea389752a2091434
yanguangkun <yanguangkun@126.com>
2014-05-28 08:53:54 +0800

    新增模块

--- webapp/js/ckeditor/config.js
+++ webapp/js/ckeditor/config.js
@@ -7,4 +7,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.font_names = "微软雅黑/微软雅黑;" + config.font_names;
 };
